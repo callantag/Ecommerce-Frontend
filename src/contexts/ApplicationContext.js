77786961b1f7f9bfc0dc9e9b77a458df3a51643f
Grace Callanta <callantag@gmail.com>
2021-05-28 13:50:54 +0800

    Use useEffect to fetch data from database

--- src/contexts/ApplicationContext.js
+++ src/contexts/ApplicationContext.js
@@ -1,26 +1,21 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const ApplicationContext = createContext()
 
 export default function ApplicationProvider(props) {
 
-	const products = [
-		{
-			_id: 1,
-			name: "Brand X",
-			description: "descX"
-		},
-		{
-			_id: 2,
-			name: "Brand Y",
-			description: "descY"
-		},
-		{
-			_id: 3,
-			name: "Brand Z",
-			description: "descZ"
-		}
-	]
+	const [products, setProducts] = useState([])
+
+	//Fetch
+	// useEffect(<function to run>, <array of state you are watching>)
+	useEffect(() => {
+		fetch("http://localhost:4000/api/products")
+		.then(res=> res.json())
+		.then(data=>{
+			setProducts(data)
+		})
+		.catch(err=>console.log(err))
+	})
 
 	return (
 		<ApplicationContext.Provider
@@ -30,3 +25,25 @@ export default function ApplicationProvider(props) {
 		</ApplicationContext.Provider>
 	)
 }
+
+
+
+
+
+// const products = [
+	// 	{
+	// 		_id: 1,
+	// 		name: "Brand X",
+	// 		description: "descX"
+	// 	},
+	// 	{
+	// 		_id: 2,
+	// 		name: "Brand Y",
+	// 		description: "descY"
+	// 	},
+	// 	{
+	// 		_id: 3,
+	// 		name: "Brand Z",
+	// 		description: "descZ"
+	// 	}
+	// ]
